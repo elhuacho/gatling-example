c7174f74b52ffea491d510ab4156e922cb94d3db
Walter <U201620367@upc.edu.pe>
2019-01-20 21:32:29 -0500

    results advance tutorial

---
+++ target/gatling/basicsimulation-20190121015329860/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FF9D00',
+name: 'All Users',
+data: [
+  [1548035612000,1],[1548035613000,1],[1548035614000,1],[1548035615000,1],[1548035616000,1],[1548035617000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
